92ff256bf3747d0760e4f5459def3c7a5fa1f240
Vandivier <john@afterecon.com>
2019-11-10 23:20:01 -0500

    feat: prettier
    and rip auth routes from ngx-admin

--- README.md
+++ README.md
@@ -13,4 +13,8 @@ Run time assumes LAMP WP install. Run `npm build` then drop the contents of `ui/
 ## Misc Notes
 
 1. Currently targeting [Angular Material](https://material.angular.io/guide/theming).
+   1. https://material.angular.io/
+   2. https://www.primefaces.org/primeng/#/
+   3. https://ionicons.com/
+   4. https://github.com/akveo/ngx-admin
 2. Login at `mysite.com/wp-admin`
--- ladderly-ui/src/app/app-routing.module.ts
+++ ladderly-ui/src/app/app-routing.module.ts
@@ -1,11 +1,45 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', pathMatch: 'full' }, // home
+    {
+        path: 'auth',
+        // component: NbAuthComponent,
+        children: [
+            {
+                path: '',
+                // component: NbLoginComponent
+            },
+            {
+                path: 'login',
+                // component: NbLoginComponent
+            },
+            {
+                path: 'register',
+                // component: NbRegisterComponent
+            },
+            {
+                path: 'logout',
+                // component: NbLogoutComponent
+            },
+            {
+                path: 'request-password',
+                // component: NbRequestPasswordComponent
+            },
+            {
+                path: 'reset-password',
+                // component: NbResetPasswordComponent
+            },
+        ],
+    },
+    // { path: "blog" },
+    { path: 'profile' },
+    { path: '**', redirectTo: 'pages' }, // 404
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
